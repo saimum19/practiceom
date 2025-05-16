eafada230b1558a71c179a9626b0205abce8b452
Omar Faruk <143751344+saimum19@users.noreply.github.com>
2025-05-16 22:04:11 +0600

    Update Bfs.java

--- selection_sort/src/selection_sort/Bfs.java
+++ selection_sort/src/selection_sort/Bfs.java
@@ -31,6 +31,9 @@ public class Bfs {
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+         for(int i= 0 ; i<n ; i++){
+               System.out.print("BFS: ");
+         }
         graph.get(0).add(1);
         graph.get(0).add(2);
         graph.get(1).add(3);
